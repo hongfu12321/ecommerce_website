70f26e83a6c1aeb9b5775afc337e1edfd6c43a42
fhong <hongfu1220@gmail.com>
2020-10-28 13:39:23 -0700

    replace placeholder in templates, and connect it to back-end

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render
 
+from .models import Customer, Product, Order, OrderItem, ShippingAddress
+
 # Create your views here.
 
 def store(request):
-    context = {}
+    product = Product.objects.all()
+    context = {'products': product}
     return render(request, 'store/store.html', context)
 
+
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, create = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #Create empty cart for now for none-logged in users
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+
+    context = {
+        "items": items,
+        "order": order,
+    }
     return render(request, 'store/cart.html', context)
     
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, create = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #Create empty cart for now for none-logged in users
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+
+    context = {
+        "items": items,
+        "order": order,
+    }
     return render(request, 'store/checkout.html', context)
     
