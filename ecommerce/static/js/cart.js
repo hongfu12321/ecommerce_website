fc1f9f10470b666816aecef9e64d4b039d1aa3d2
fhong <hongfu1220@gmail.com>
2020-10-30 17:23:08 -0700

    update logic for unauthenticated user using browser cookies

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -8,12 +8,39 @@ for (i = 0; i < updateBtns.length; i++){
         
         console.log('USER', user)
         if (user == 'AnonymousUser')
-            console.log('User is not authemticated')
+            addCookieItem(productId, action)
         else
             updateUserOrder(productId, action)
-
+        
     })
 }
+    
+function addCookieItem(productId, action){
+    console.log('User is not authemticated')
+    
+    if (action == 'add'){
+        if (cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        }
+        else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+    if (action == 'remove'){
+        car[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0){
+            console.log('Item should be deleted')
+            delete cart[productId];
+        }
+    }
+
+    console.log('CART: ', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domin=;path=/"
+
+    location.reload()
+}
+
 
 function updateUserOrder(productId, action){
     console.log('User is authenticated, sending data...')
